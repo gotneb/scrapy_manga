8bee7772ccdde5fc76bb0d798dbb97a1a203a1b1
Gabriel Bento <67703889+gotneb@users.noreply.github.com>
2022-12-22 14:15:06 -0400

    Update main.py
    Testing render

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from core.sites.readm import get_manga
+from core.sites.readm import get_manga, get_populars
+import logging, time
 
 if __name__ == "__main__":
-    manga = get_manga("https://readm.org/manga/one-piece")
-    manga.show()
+    ONE_MINUTE = 60
+    logging.info("STARTING...")
+    while True:
+        manga = get_manga("https://readm.org/manga/versatile-mage")
+        manga.show()
+        time.sleep(2 * ONE_MINUTE)
