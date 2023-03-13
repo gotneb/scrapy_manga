a7bcda2a3b7ef2f9134c8e1abdc8e0d0a0f883a1
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-03-13 12:33:38 -0400

    Add function to extract links
    `get_all_start_with()` function extracts all manga links that starts with a specific letter.

--- core/sites/readm.py
+++ core/sites/readm.py
@@ -1,3 +1,5 @@
+from typing import Callable, NoReturn
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
@@ -5,17 +7,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 from core.manga import Manga
 
 
-def get_driver(display_gui) -> webdriver.Chrome:
+def get_driver(show_window) -> webdriver.Chrome:
     """
-        Creates a webdriver. If `display_gui` is true, then display chrome window.  
+    Creates a webdriver. If `show_window` is true, then display chrome window.\n
     Arguments:  
-        display_gui: bool
+        show_window: shows google chrome's window.
     Returns:
-        A google's webdriver
+        A google's webdriver.
     """
     # TODO: Throw exception if chrome is not installed
     options = webdriver.ChromeOptions()
-    if not display_gui:
+    if not show_window:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
@@ -23,9 +25,7 @@ def get_driver(display_gui) -> webdriver.Chrome:
 
 # TODO: Make it avaliable for "mangalivre.net" as well
 def get_populars() -> list[Manga]:
-    """
-    Visits the `readm.org` and returns top 10 most populars mangas.
-    """
+    """Visits the `readm.org` and returns top 10 most populars mangas."""
     driver = get_driver()
     url = "https://readm.org/popular-manga"
     driver.get(url)
@@ -46,8 +46,38 @@ def get_populars() -> list[Manga]:
     return mangas
 
 
+def get_all_start_with(letter, show_window=True, on_link_received: Callable[[str], NoReturn]=None) -> list[str]:
+    """
+    Visits `readm.org` and extract all links that starts with `letter` on its name.\n
+    Arguments:
+        letter: manga initial name.
+        show_window: show google's chrome window.
+        on_link_received: callback that's called when manga's link is received.
+    Return:
+        A list containing all links.
+    """
+    if len(letter) > 2:
+        raise Exception('letter must be unique character.')
+
+    letter = letter.lower()
+    driver = get_driver(show_window)
+    driver.get(f'https://readm.org/manga-list/{letter}')
+
+    # Get all tags '<a>'
+    all_links = []
+    anchors = driver.find_elements(By.CSS_SELECTOR, 'li div.poster.poster-xs a')
+    for a in anchors:
+        link = a.get_attribute('href')
+        all_links.append(link)
+        # Callback
+        if on_link_received is not None:
+            on_link_received(link)
+    
+    return all_links
+
+
 # FIX: Disabling chrome's window may throw errors
-def manga_detail(manga_url, enable_gui=True) -> Manga:
+def manga_detail(manga_url, show_window=True) -> Manga:
     """
     Visits the `manga_url` and extract all data on it.
     Arguments:
@@ -56,7 +86,7 @@ def manga_detail(manga_url, enable_gui=True) -> Manga:
     Return:
         Manga content.
     """
-    driver = get_driver(display_gui=enable_gui)
+    driver = get_driver(show_window)
     driver.get(manga_url)
 
     # Just for debug...
