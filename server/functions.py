acc4408ec103b2b2620e24fb0299f1f924a22b69
andregirao05 <andregirao05@gmail.com>
2023-03-15 15:18:50 -0400

    feat: add function add_manga_details to server/functions

--- server/functions.py
+++ server/functions.py
@@ -7,6 +7,18 @@ from core.sites.readm import manga_detail, get_pages
 db = client();
 details_colection = db.collection("mangas").document("readm").collection("details")
 
+def add_manga_details(manga: Manga):
+    """
+    Add a new manga details to the database
+    Returns:
+        A reference to the created document
+    """
+    manga_dict = manga.to_dict()
+    if manga_exists(manga_dict['title']):
+        return None
+    doc_ref = details_colection.add(manga_dict)
+    return doc_ref
+
 def get_manga_details(title: str):
     """returns the first document from database with same title"""
     docs = details_colection.where("title", "==", title).stream()
@@ -15,5 +27,6 @@ def get_manga_details(title: str):
     return None
 
 def manga_exists(title: str):
+    """returns True if manga exists in database"""
     doc = get_manga_details(title)
     return doc != None
