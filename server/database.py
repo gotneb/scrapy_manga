d17441b70ff3c1061e4d3cf023ae1bd87e934834
andregirao05 <andregirao05@gmail.com>
2023-03-16 15:38:18 -0400

    refactor: changes method name from manga_exists to manga_exists_by_title

--- server/database.py
+++ server/database.py
@@ -34,7 +34,7 @@ class MangaDatabase:
             A reference to the created document
         """
         manga_dict = manga.to_dict()
-        if self.manga_exists(manga_dict['title']):
+        if self.manga_exists_by_title(manga_dict['title']):
             return None
         _, doc_ref = self.details_collection.add(manga_dict)
         return doc_ref.id
@@ -62,7 +62,7 @@ class MangaDatabase:
             return doc.id, doc.to_dict()
         return None
 
-    def manga_exists(self, title: str) -> bool:
+    def manga_exists_by_title(self, title: str) -> bool:
         """returns True if manga exists in database"""
         doc = self.get_details_by_title(title)
         return doc != None
