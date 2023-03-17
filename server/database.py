0eb9280e678b2e8426f2194a0dbf00ecb09c79ff
andregirao05 <andregirao05@gmail.com>
2023-03-17 14:26:47 -0400

    refact: move ChapterEntity to file /server/entities.py

--- server/database.py
+++ server/database.py
@@ -1,22 +1,8 @@
 from firebase_admin.firestore import client
 from core.manga import Manga
+from entities import ChapterEntity
 
 # Firestore Python docs: https://cloud.google.com/python/docs/reference/firestore/latest
-
-"""Class used to stored manga chapter in the database"""
-
-class ChapterEntity:
-    def __init__(self, manga_id: str, chapter_num: str, image_urls: list[str]) -> None:
-        self.manga_id = manga_id
-        self.chapter_num = chapter_num
-        self.image_urls = image_urls
-    
-    def to_dict(self) -> dict:
-        return {
-            'manga_id': self.manga_id,
-            'chapter_num': self.chapter_num,
-            'image_urls': self.image_urls
-        }
 
 class MangaDatabase:
     def __init__(self):
