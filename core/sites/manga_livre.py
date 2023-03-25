6fc18e8309b39e78db2d89e7179228d955988a68
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-03-25 11:43:05 -0400

    Implement: `get_chapters`

--- core/sites/manga_livre.py
+++ core/sites/manga_livre.py
@@ -21,22 +21,30 @@ def manga_detail(manga_url, show_window=True):
     driver = init_driver(show_window)
     driver.get(manga_url)
 
-    # Just for debug...
-    # print(f"Openned {driver.title}")
+    # "Manga Livre" doesn't have a artist
+    artist = None
 
     title = get_title(driver)
     genres = get_genres(driver)
     author = get_author(driver)
     summary = get_summary(driver)
     thumbnail = get_thumbnail(driver)
-    # TODO: Get chapters
-    chapters = ['']
-    #chapters = get_chapters(driver)
+    chapters = get_chapters(driver)
+    total_chapters = len(chapters)
 
     # Clean resources
     driver.quit()
 
-    return Manga(title, 'alt_title', author, 'artist', thumbnail, genres, summary, 'stt', 'total_chapters', chapters)
+    return Manga(title=title, 
+                 alternative_title='alt_title', 
+                 author=author, 
+                 artist=artist, 
+                 thumbnail=thumbnail, 
+                 genres=genres, 
+                 summary=summary, 
+                 status='stt', 
+                 total_chapters=total_chapters, 
+                 chapters=chapters)
 
 
 def get_thumbnail(driver: webdriver.Chrome):
@@ -75,7 +83,7 @@ def get_summary(driver: webdriver.Chrome) -> str:
 
 
 def get_genres(driver: webdriver.Chrome) -> list[str]:
-    # kkkk I don't even know if it's allowed type that way :D
+    # Is that way wrong? I mean... I don't think this kinda syntax looks like weird :P
     elems = driver.find_elements(
     By.CSS_SELECTOR,
     'div#series-data.content-wraper div.series-info.touchcarousel div.touchcarousel-wrapper ul.tags.touchcarousel-container li')
@@ -83,27 +91,48 @@ def get_genres(driver: webdriver.Chrome) -> list[str]:
     genres = []
     for li in elems:
         genres.append(li.text)
+    
+    return genres
 
 
 def get_chapters(driver: webdriver.Chrome):
-    # TODO: scroll the entire content
-    while True:
+    # PROBLEM:
+    # Chapters are loaded lazily by AJAX.
+    # There isn't a specific time where I can know in advance when the page has fully loaded.
+    # 
+    # HOW IT WORKS:
+    # There's a specific div notifying user when content is being loaded
+    # Every time we scroll this div, it updates its children "style"
+    # When it's done, all chidren have block style "none", it loads about 250 chapters...
+    # So I've putted a "limiter". I think 50 it's a great amount.
+    #
+    # DON'T WORRY: 
+    # Once all chapters were loaded, the div children will have the same display
+    # Meaning the counter will quickly increment to "limit".
+
+    limit = 50
+    counter = 0
+    while counter < limit:
+        # Scroll the page indefinitely
         ActionChains(driver)       \
         .scroll_by_amount(0, 1000) \
         .perform()
-        #time.sleep(1/10)
 
+        # Site notify user chapters are being loaded
         first = driver.find_element(By.CSS_SELECTOR, 'div.loadmore a.loadmore')
         second = driver.find_element(By.CSS_SELECTOR, 'div.loadmore a.loading.loadmore')
-
         st1 = first.get_attribute('style')
         st2 = second.get_attribute('style')
 
         if st1 == 'display: none;' and st1 == st2:
-            print(f'1 | {st1}\n2 | {st2}')
+            counter += 1
 
+    # When fyllu loaded, we can extract data
     elems = driver.find_elements(By.CSS_SELECTOR, 'div.container-box.default.color-brown ul.full-chapters-list.list-of-chapters li a.link-dark')
+    chapters = []
     for a in elems:
         # title's attribute returns: 'Ler Capitulo `N`', where N is a number =P
-        title = a.get_attribute('title').split(' ')[2] 
-        print(title)
+        chapter_value = a.get_attribute('title').split(' ')[2] 
+        chapters.append(chapter_value)
+    
+    return chapters
