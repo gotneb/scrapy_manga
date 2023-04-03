c26f61b76ce1605105008fadf36042b7db9b3e5e
andregirao05 <andregirao05@gmail.com>
2023-04-03 16:13:41 -0400

    fix: change Manga in server/entities.py

--- server/entities.py
+++ server/entities.py
@@ -10,12 +10,14 @@ class Manga:
     author: str
     artist: str
     status: str
+    url: str
     origin: str
     language: str
     total_chapters: int
     thumbnail: str
     genres: list[str]
     summary: str
+    chapters_names: list[str]
     chapters: dict
 
     def to_dict(self):
@@ -26,17 +28,19 @@ class Manga:
     def dict_to_manga(cls, manga_dict: dict):
         if manga_dict is not None:
             return Manga(
-                manga_dict["title"],
-                manga_dict["alternative_title"],
-                manga_dict["author"],
-                manga_dict["artist"],
-                manga_dict["status"],
-                manga_dict["origin"],
-                manga_dict["language"],
-                manga_dict["total_chapters"],
-                manga_dict["thumbnail"],
-                manga_dict["genres"],
-                manga_dict["summary"],
-                manga_dict["chapters"],
+                title=manga_dict["title"],
+                alternative_title=manga_dict["alternative_title"],
+                author=manga_dict["author"],
+                artist=manga_dict["artist"],
+                status=manga_dict["status"],
+                url=manga_dict["url"],
+                origin=manga_dict["origin"],
+                language=manga_dict["language"],
+                total_chapters=manga_dict["total_chapters"],
+                thumbnail=manga_dict["thumbnail"],
+                genres=manga_dict["genres"],
+                summary=manga_dict["summary"],
+                chapters_names=manga_dict["chapters_names"],
+                chapters=manga_dict["chapters"],
             )
         return None
