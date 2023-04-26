31f65eb7ffdd4efeb01fba7bf9899f1118b3566f
andregirao05 <andregirao05@gmail.com>
2023-04-26 19:02:34 -0400

    feat: site update schedule

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from server import ReadmHandler, MangaDatabase
+from server import ReadmHandler, MangaDatabase, Database
+import schedule
+from time import sleep
+from random import randomint
 
-if __name__ == "__main__":
+
+def update_sites():
     db = MangaDatabase()
 
     db.connect()
@@ -10,3 +14,13 @@ if __name__ == "__main__":
     handler.join()
 
     db.close()
+
+
+if __name__ == "__main__":
+    hour = 23
+    minutes = randomint(0, 60)
+    schedule.every().day.at("{:02d}:{:02d}".format(hour, minutes)).do(update_sites)
+
+    while True:
+        schedule.run_pending()
+        sleep(60 * 60)
