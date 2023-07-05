bc1e503ac9b5164bc33a35a1452a4f60949f202c
andregirao05 <andregirao05@gmail.com>
2023-07-05 13:35:56 -0400

    fix: create log file in same scripts directory

--- execution/log_configs.py
+++ execution/log_configs.py
@@ -1,10 +1,15 @@
 import logging
+import os
 
 log_file_name = "exec_logs.log"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+log_file_path = os.path.join(parent_dir, log_file_name)
 
 # create and configure logger
 logging.basicConfig(
-    filename=log_file_name, format="%(asctime)s %(message)s", filemode="w"
+    filename=log_file_path, format="%(asctime)s %(message)s", filemode="w"
 )
 
 logger = logging.getLogger()
