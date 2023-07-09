04f43e29a7f7e47138a8bc6cda4699aa95a8fe5a
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-07-09 14:31:02 -0400

    fix: user-data-dir static wrong path

--- core/driver.py
+++ core/driver.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+import getpass
 
 _profile = 'selenium_manga'
+_username = getpass.getuser()
 
 def init_driver(show_window) -> webdriver.Chrome:
     """
@@ -11,7 +13,7 @@ def init_driver(show_window) -> webdriver.Chrome:
         A google's webdriver.
     """
     options = webdriver.ChromeOptions()
-    options.add_argument(f"user-data-dir=/home/gabriel/.config/google-chrome/{_profile}")
+    options.add_argument(f'user-data-dir=/home/{_username}/.config/google-chrome/{_profile}')
 
     if not show_window:
         options.add_argument('--headless')
