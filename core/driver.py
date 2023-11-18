1e5c531f2fe4f9e2e3aaca1d39cdddb82da4e7cd
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-11-18 17:54:58 -0400

    feat: add functions to get `driver` HTML content

--- core/driver.py
+++ core/driver.py
@@ -10,14 +10,15 @@ def create_folder_if_not_exists(folder_path):
         os.makedirs(folder_path)
 
 
-def init_driver(show_window) -> webdriver.Firefox:
+def init_driver(show_window, timeout: int = 3) -> webdriver.Firefox:
     """
     Creates a webdriver.\n
     Arguments:  
         `show_window:` display firefox's window.
     Returns:
-        A google's webdriver.
+        A firefox's webdriver.
     """
+    # Unix like only
     profile_path = f'/home/{_username}/.mozilla/firefox/{_profile}'
     create_folder_if_not_exists(profile_path)
 
@@ -29,4 +30,16 @@ def init_driver(show_window) -> webdriver.Firefox:
     if not show_window:
         options.add_argument('-headless')
 
-    return webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(options=options)
+    driver.set_page_load_timeout(timeout)
+    return driver
+
+
+def get_driver_html(driver: webdriver.Firefox, url: str) -> str:
+    """Returns the HTML content from a given driver."""
+    try:
+        driver.get(url)
+    except:
+        driver.execute_script("window.stop();")
+
+    return driver.page_source
