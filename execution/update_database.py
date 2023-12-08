9f96800d6e7685a9c74d16ffb33d0ff7677f497f
andregirao05 <andregirao05@gmail.com>
2023-12-08 12:36:18 -0400

    refactor: chenge updates order

--- execution/update_database.py
+++ execution/update_database.py
@@ -14,10 +14,10 @@ def update_database(args):
     number_of_works = args["number_of_works"]
     exec_all = args["exec_all"]
 
-    if "nine_manga_br" in website_list:
-        update_mangas_nine(number_of_works, exec_all)
-
     if "readm" in website_list:
         update_mangas_from_readm(number_of_works, exec_all)
+
+    if "nine_manga_br" in website_list:
+        update_mangas_nine(number_of_works, exec_all)
 
     logger.info("Stoping the site update process")
