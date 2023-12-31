5dd3f82ddf308b3ff2acbfe064d51ae601016577
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-12-31 15:00:18 -0400

    wip: getting chapters

--- core/sites/mangadex_br/manga.py
+++ core/sites/mangadex_br/manga.py
@@ -1,8 +1,8 @@
+import math
 from requests import get
+from entities.chapter import Chapter
 from entities.manga import Manga
 from .constants import base_url
-
-from utils.debug_tools import save_text
 
 def get_manga(manga_id: str):
     """
@@ -15,16 +15,20 @@ def get_manga(manga_id: str):
 
     #print(json['data']['attributes']['description']['en'])
 
-    title = json['data']['attributes']['title']['en']
+    # Should I use the raw or the mangadex url?
+    url = json['data']['attributes']['links']['raw']
+    # I think it could be better using a list
     alt_title = get_alt_titles(json)
+
+    title = json['data']['attributes']['title']['en']
     author = get_author(json)
     status = json['data']['attributes']['status']
-    artist = ''
-    score = ''
+    artist = None
+    score = get_score(manga_id)
     thumbnail = get_thumbnail(manga_id, json)
     genres = get_genres(json)
     summary = json['data']['attributes']['description']['en']
-    chapters = ''
+    chapters = get_chapters(manga_id)
 
     return Manga(
         title=title,
@@ -32,7 +36,7 @@ def get_manga(manga_id: str):
         author=author,
         artist=artist,
         status=status,
-        url='',
+        url=url,
         origin='mangadex_br',
         language='portuguese',
         thumbnail=thumbnail,
@@ -49,6 +53,16 @@ def get_author(json) -> str:
 
     r = get(f'{base_url}/author/{author_id}')
     return r.json()['data']['attributes']['name']
+
+
+def get_artist(json) -> str:
+    json['data']['relationships'][3]['id']
+    artist_id = json['data']['relationships'][0]['id']
+
+    r = get(f'{base_url}/artist/{artist_id}')
+    print(r.content.decode())
+    artist = r.json()['data']['attributes']['name']
+    print(artist)
 
 
 def get_thumbnail(manga_id: str, json):
@@ -73,3 +87,53 @@ def get_alt_titles(json):
 def get_genres(json):
     tags = json['data']['attributes']['tags']
     return [tag['attributes']['name']['en'] for tag in tags]
+
+
+def get_score(manga_id: str):
+    r = get(f'{base_url}/statistics/manga/{manga_id}')
+    sc = r.json()['statistics'][manga_id]['rating']['average']
+    return float(sc)
+
+
+def get_chapters(manga_id: str):
+    chapters = []
+
+    r = get(
+        f"{base_url}/manga/{manga_id}/feed",
+        params={
+            'limit': 100,
+            'offset': 0,
+            'translatedLanguage[]': ['pt-br'],
+        }
+    )
+
+    json = r.json()
+    total = math.ceil(int(json['total']) / 100)
+
+    for i in range(0, total):
+        r = get(
+            f"{base_url}/manga/{manga_id}/feed",
+            params={
+                'limit': 100,
+                'offset': i * 100,
+                'translatedLanguage[]': ['pt-br'],
+            }
+        )
+    
+        json = r.json()
+        for value in json['data']:
+            c = json2chapter(value)
+            chapters.append(c)
+    
+    # From the api pagination, the chapters come randomly...
+    chapters.sort(key=lambda c: float(c.name))
+    
+    return chapters
+
+
+def json2chapter(json) -> Chapter:
+    return Chapter(
+        name=json["attributes"]['chapter'],
+        title=json["attributes"]['title'],
+        pages=[0]
+    )
--- entities/chapter.py
+++ entities/chapter.py
@@ -1,15 +1,19 @@
-from dataclasses import dataclass
 from .entity import Entity
 
 
 class Chapter(Entity):
-    def __init__(self, name: str, pages: list[str] = [], id: str | None = None) -> None:
+    def __init__(self, name: str, title: str, pages: list[str] = [], id: str | None = None) -> None:
         self.name: str = name
+        self.title: str | None = title
         self.pages: list[str] = pages
         self.id: str | None = id
 
     def to_dict(self) -> dict:
-        return {"name": self.name, "pages": self.pages}
+        return {
+            "name": self.name, 
+            "title": self.title, 
+            "pages": self.pages
+        }
 
     def is_empty(self) -> bool:
         return len(self.pages) == 0
